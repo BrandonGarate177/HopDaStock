2f3037e805ec68a9037837e529d06859ea70d00f
BrandonGarate177 <brandongarate177@gmail.com>
2024-11-07 16:28:25 -0800

    Initial Commit

---
+++ HopDaStock/HopDaStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopDaStockApp.swift
+//  HopDaStock
+//
+//  Created by Brandon Garate on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HopDaStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
